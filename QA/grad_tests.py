fixed jacobian test for resnet

QA/grad_tests.py
@@ -7,7 +7,6 @@ from scipy.io import loadmat
 from nn.loss import CrossEntropy
 from nn.layer import Layer, ResNetLayer
 from nn.activations import Tanh, SoftMax
-from nn import NeuralNetwork
 from nn.utils import pad
 from nn.utils.tensor import Tensor
 from utils import nn_builder
@@ -171,13 +170,13 @@ def grad_test_model(X, C, resnet=False):
 def resnet_jacobian_test_W1(X, C):
     in_shape = X.shape[0]
     out_shape = X.shape[0]
-    d = np.random.randn(in_shape + 1, out_shape)
+    layer = ResNetLayer(in_shape, out_shape, Tanh())
+    W1: Tensor = layer.W1.view()
+    d = np.random.randn(*W1.shape)
     d /= np.linalg.norm(d)
     n_iter = 10
     grad_errs = []
     no_grad_errs = []
-    layer = ResNetLayer(in_shape, out_shape, Tanh())
-    W1: Tensor = layer.W1.view()
     W2: Tensor = layer.W2.view()
     for i in range(1, n_iter + 1):
         eps = 1e-2 * (0.5 ** i)
@@ -194,7 +193,7 @@ def resnet_jacobian_test_W1(X, C):
 
         err = left_A - right_A
         dA = layer.activation.grad(layer.inner_A)
-        diff = eps * W2.T @ ((d.T @ pad(X)) * dA)
+        diff = eps * W2 @ ((d.T @ pad(X)) * dA)
         no_grad_abs_err: float = np.linalg.norm(err)
         grad_abs_err = np.linalg.norm(err - diff)
         no_grad_errs.append(no_grad_abs_err)
@@ -203,40 +202,32 @@ def resnet_jacobian_test_W1(X, C):
     
     plot_test(grad_errs, no_grad_errs, 'ResNet W1 Jacobian Test')
 
-
 def resnet_jacobian_test_W2(X, C):
     in_shape = X.shape[0]
     out_shape = X.shape[0]
-    d = np.random.randn(out_shape, X.shape[1])
+    layer = ResNetLayer(in_shape, out_shape, Tanh())
+    W2: Tensor = layer.W2.view()
+    d = np.random.randn(*W2.shape)
     d /= np.linalg.norm(d)
     n_iter = 10
     grad_errs = []
     no_grad_errs = []
-    layer = ResNetLayer(in_shape, out_shape, Tanh())
-
-    # f = resnet = X + W2 @ activation(W1 @ X)
-    # grad(f) = dA/dZ * dZ/dW2 # Z = W2 @ A_inner
-    # f = ||resnet(w + eps * d) - resnet(w)||
-    # df/dW2 = df/A * dA/W2 = 2 * (resnet(w + eps * d) - resnet(w)) * dA/W2
-
 
     for i in range(1, n_iter + 1):
         eps = 1e-2 * (0.5 ** i)
 
         # Compute left term
-        # layer.W2 += eps * d
-        
-        left_A = layer(X + eps * d)#layer.A.copy()
+        layer.W2 += eps * d
+        layer(X)
+        left_A = layer.A.copy()
 
         # Compute right term
-        # layer.W2 -= eps * d
-        
-        right_A = layer(X)#layer.A.copy()
+        layer.W2 -= eps * d
+        layer(X)
+        right_A = layer.A.copy()
 
         err = left_A - right_A
-        # layer.backward(X, 2 * err)
-        diff = eps * (d.T @ layer.W2).T
-        # diff = (eps * d * layer.W.grad[:-1, :, 1]) @ layer.inner_A / eps
+        diff = eps * d @ layer.inner_A
         no_grad_abs_err: float = np.linalg.norm(err)
         grad_abs_err = np.linalg.norm(err - diff)
         no_grad_errs.append(no_grad_abs_err)
@@ -245,14 +236,47 @@ def resnet_jacobian_test_W2(X, C):
     
     plot_test(grad_errs, no_grad_errs, 'ResNet W2 Jacobian Test')
 
+def resnet_jacobian_test_X(X, C):
+    in_shape = X.shape[0]
+    out_shape = X.shape[0]
+    layer = ResNetLayer(in_shape, out_shape, Tanh())
+    W1: Tensor = layer.W1.view()
+    W2: Tensor = layer.W2.view()
+    d = np.random.randn(*X.shape)
+    d /= np.linalg.norm(d)
+    n_iter = 10
+    grad_errs = []
+    no_grad_errs = []
+
+    for i in range(1, n_iter + 1):
+        eps = 10 * (0.5 ** i)
+
+        # Compute left term
+        left_A = layer(X + eps * d)
+
+        # Compute right term
+        right_A = layer(X)
+
+        err = left_A - right_A
+        dA = layer.activation.grad(layer.inner_A)
+        diff = eps * (d + W2 @ (dA * (W1[:-1, :].T @ d)))
+        no_grad_abs_err: float = np.linalg.norm(err)
+        grad_abs_err = np.linalg.norm(err - diff)
+        no_grad_errs.append(no_grad_abs_err)
+        grad_errs.append(grad_abs_err)
+
+    
+    plot_test(grad_errs, no_grad_errs, 'ResNet X Jacobian Test')
 
 
 if __name__ == '__main__':
     Data = loadmat('Data/GMMData.mat')
     X, C = Data['Yt'], Data['Ct']
     os.makedirs('./figures', exist_ok=True)
-    # resnet_jacobian_test_W2(X, C)
+    resnet_jacobian_test_X(X, C)
+    resnet_jacobian_test_W1(X, C)
+    resnet_jacobian_test_W2(X, C)
     grad_test_model(X, C, resnet=True)
-    # jacobian_test_W(X, C)
-    # jacobian_test_X(X, C)
-    # gradient_test(X, C)
+    jacobian_test_W(X, C)
+    jacobian_test_X(X, C)
+    gradient_test(X, C)

QA/softmax_regression.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import optuna
 import os
 
 from scipy.io import loadmat
@@ -11,12 +10,18 @@ from nn import NeuralNetwork
 from train import train_epoch
 from metrics import accuracy
 
-def objective(trial):
-    batch_size = trial.suggest_categorical('batch_size', [2 ** i for i in range(10)])
-    lr = trial.suggest_float('lr', 1e-5, 1)
 
-    print(f"Starting trial {trial.number} with params {trial.params}")
-    epochs = 100
+if __name__ == "__main__":
+    data = loadmat('Data/SwissRollData.mat')
+    X_train, X_test = data['Yv'], data['Yt']
+    C_train, C_test = data['Cv'], data['Ct']
+    in_shape = X_train.shape[0]
+    out_shape = C_train.shape[0]
+    os.makedirs('./figures', exist_ok=True)
+
+    lr, batch_size = 1e-2, 32
+
+    epochs = 1000
     model = NeuralNetwork()
     model.add_layer(in_shape, out_shape, 'softmax')
     optimizer = SGD(lr, momentum=0.9)
@@ -30,44 +35,12 @@ def objective(trial):
         train_accs.append(acc)
         val_accs.append(val_acc)
     
-    return val_accs[-1]
-
-
-
-if __name__ == "__main__":
-    data = loadmat('Data/SwissRollData.mat')
-    X_train, X_test = data['Yv'], data['Yt']
-    C_train, C_test = data['Cv'], data['Ct']
-    in_shape = X_train.shape[0]
-    out_shape = C_train.shape[0]
-    os.makedirs('./figures', exist_ok=True)
-
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100)
-    print(f"{study.best_params=}")
-
-    batch_size = study.best_params['batch_size']
-    lr = study.best_params['lr']
-
-    epochs = 100
-    model = NeuralNetwork()
-    model.add_layer(in_shape, out_shape, 'softmax')
-    optimizer = SGD(lr, momentum=0.9)
-    loss_fn: CrossEntropy = CrossEntropy()
-    train_accs, val_accs = [], []
-
-    for _ in tqdm(range(epochs), desc="Training"):
-        train_epoch(model, optimizer, loss_fn, X_train, C_train, batch_size)
-        acc = accuracy(model(X_train), C_train)
-        val_acc = accuracy(model(X_test), C_test)
-        train_accs.append(acc)
-        val_accs.append(val_acc)
-
     plt.plot(range(epochs), train_accs, label='Train')
     plt.plot(range(epochs), val_accs, label='Validation')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
+    plt.title(f'Softmax Regression {lr=}, {batch_size=}')
 
     plt.savefig('./figures/softmax_regression.png')
     plt.show()

nn/activations.py
@@ -1,89 +1,123 @@
 import numpy as np
-
 from abc import ABC, abstractmethod
-
 
 class Activation(ABC):
 
-    def __init__(self):
-        pass
-
-    def __call__(self, Z: np.ndarray):
-        return self.activation(Z)
-
+    @staticmethod
     @abstractmethod
-    def activation(self, Z: np.ndarray) -> np.ndarray:
+    def activation(Z: np.ndarray) -> np.ndarray:
         """
         Applies an activation function to the input array.
+
         Parameters:
-            A (np.ndarray): The input array to which the activation function will be applied.
+            Z (np.ndarray): The input array to which the activation function will be applied.
+
         Returns:
             np.ndarray: The result of applying the activation function to the input array.
         """
-
         pass
 
+    @staticmethod
     @abstractmethod
-    def grad(self, A: np.ndarray) -> np.ndarray:
+    def grad(A: np.ndarray) -> np.ndarray:
         """
         Compute the gradient of the activation function.
-        
+
+        Parameters:
+            A (np.ndarray): The input array for which the gradient is to be computed.
+
         Returns:
             np.ndarray: Gradient of the activation function.
         """
         pass
 
+    def __call__(self, Z: np.ndarray) -> np.ndarray:
+        return self.activation(Z)
+
 
 class Tanh(Activation):
-    def activation(self, Z: np.ndarray):
+
+    @staticmethod
+    def activation(Z: np.ndarray) -> np.ndarray:
         """
-        Perform the forward pass of the neural network layer.
-        
+        Perform the forward pass using the tanh activation function.
+
+        Parameters:
+            Z (np.ndarray): Input array.
+
         Returns:
-            np.ndarray: Output of the layer after applying the tanh activation function.
+            np.ndarray: Output array after applying the tanh activation function.
         """
         return np.tanh(Z)
 
-    def grad(self, A: np.ndarray):
-        return (1 - A ** 2)
+    @staticmethod
+    def grad(A: np.ndarray) -> np.ndarray:
+        """
+        Compute the gradient of the tanh activation function.
 
+        Parameters:
+            A (np.ndarray): Input array.
+
+        Returns:
+            np.ndarray: Gradient of the tanh activation function.
+        """
+        return 1 - A ** 2
 
 class ReLU(Activation):
-    def activation(self, Z: np.ndarray):
+
+    @staticmethod
+    def activation(Z: np.ndarray) -> np.ndarray:
         """
-        Applies the ReLU (Rectified Linear Unit) activation function to the input array.
-        The ReLU function sets all negative values in the input array to zero.
+        Applies the ReLU activation function to the input array.
+
         Parameters:
-        A (np.ndarray): Input array to which the ReLU activation function will be applied.
+            Z (np.ndarray): Input array.
+
         Returns:
-        np.ndarray: Output array with ReLU activation applied.
+            np.ndarray: Output array after applying ReLU activation.
         """
         A = Z.copy()
         A[A < 0] = 0
         return A
 
-    def grad(self, A: np.ndarray):
+    @staticmethod
+    def grad(A: np.ndarray) -> np.ndarray:
         """
-        Compute the gradient of the activation function.
+        Compute the gradient of the ReLU activation function.
+
         Parameters:
-        A (np.ndarray): The input array for which the gradient is to be computed.
+            A (np.ndarray): Input array.
+
         Returns:
-        np.ndarray: The gradient of the activation function with respect to the input array.
+            np.ndarray: Gradient of the ReLU activation function.
         """
-
-        return np.where(A, 1, 0)
-
+        return np.where(A > 0, 1, 0)
 
 class SoftMax(Activation):
-    def activation(self, Z: np.ndarray):
-        """
-        Perform the forward pass of the neural network layer.
-        
-        Returns:
-            np.ndarray: Output of the layer after applying the softmax activation function.
-        """
-        exps = np.exp(Z - np.max(Z, axis=0))
-        return exps / np.sum(exps, axis=0)
 
-    def grad(self, A: np.ndarray):
-        return np.ones_like(A) # not true, but works for the purposes of softmax here.
+    @staticmethod
+    def activation(Z: np.ndarray) -> np.ndarray:
+        """
+        Applies the softmax activation function to the input array.
+
+        Parameters:
+            Z (np.ndarray): Input array.
+
+        Returns:
+            np.ndarray: Output array after applying softmax activation.
+        """
+        exps = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+        return exps / np.sum(exps, axis=0, keepdims=True)
+
+    @staticmethod
+    def grad(A: np.ndarray) -> np.ndarray:
+        """
+        Placeholder gradient for the softmax activation function.
+
+        Parameters:
+            A (np.ndarray): Input array.
+
+        Returns:
+            np.ndarray: Gradient of the softmax activation function.
+        """
+        return np.ones_like(A)  # Placeholder; softmax gradients depend on the full input array.